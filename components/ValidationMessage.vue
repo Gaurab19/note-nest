93c0ca46a2db818dfa68c700f53bf57057d11716
Gaurab19 <gaurab625@gmail.com>
2025-04-19 00:41:48 +0545

    chore:vuelidate added for client side validation

---
+++ components/ValidationMessage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-orange-300 text-xs mt-1">
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+</script>
--- pages/register.vue
+++ pages/register.vue
@@ -25,6 +25,9 @@
               type="email"
               class="block w-full bg-[#27272A] border border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm"
           />
+          <ValidationMessage
+          >{{ v$.email?.$errors[0]?.$message }}
+          </ValidationMessage>
         </div>
 
         <div class="mt-6">
@@ -37,12 +40,15 @@
               type="password"
               class="block w-full bg-[#27272A] border border-[#3F3F46] rounded text-white px-4 py-2 placeholder:text-zinc-500 text-sm"
           />
+          <ValidationMessage
+          >{{ v$.password?.$errors[0]?.$message }}
+          </ValidationMessage>
         </div>
 
         <!-- sign up button -->
         <div>
           <button
-              class="w-full mt-4 bg-[#FFAC00] rounded-full px-4 py-2 text-sm font-bold flex justify-center items-center space-x-2"
+              class="w-full mt-4 bg-[#FFAC00] rounded-full px-4 py-2 text-sm font-bold flex justify-center items-center space-x-2 cursor-pointer"
           >
             <span>Sign Up</span>
             <ArrowRight />
@@ -53,32 +59,56 @@
     </div>
     <!-- /sidebar -->
     <!-- note introduction -->
-    <div></div>
+    <div class="flex-1 bg-zinc-800 flex items-center justify-center">
+      <div class="max-w-lg p-8">
+        <h2 class="text-white text-2xl font-bold">Welcome to NoteNest</h2>
+        <p class="text-zinc-300 mt-4">
+          Create an account to start using our Note's service and unlock all features.
+        </p>
+      </div>
+    </div>
     <!-- /note introduction -->
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { toast } from 'vue3-toastify';
+import { useVuelidate } from '@vuelidate/core'
+import { helpers,required, minLength,email as emailValidator } from '@vuelidate/validators'
 const email = ref('')
 const password = ref('')
 
-async function submit() {
-  try {
-    const response = await $fetch('/api/user', {
-      method: 'POST',
-      body: {
-        email: email.value,
-        password: password.value,
-      },
-    })
-  }catch (e){
-    // useNuxtApp().$toast.info('Hello World.\n I am <b>Tom</b>', {
-    //   autoClose: 5000,
-    //   dangerouslyHTMLString: true,
-    // });
-    toast.error(e.response?._data.message, { autoClose: 5000 });
+const rules = ref({
+  email: {
+    required: helpers.withMessage('Email is required', required),
+    email: helpers.withMessage('Please enter a valid email address', emailValidator),
+    $autodirty: true,
+  },
+  password: {
+    required: helpers.withMessage('Password is required', required),
+    minLength: helpers.withMessage('Password must be at least 8 characters', minLength(8)),
+    $autodirty: true,
   }
-
+});
+const v$ = useVuelidate(rules, { email,password })
+async function submit() {
+  v$.value.$validate();
+  if(v$.value.$errors.length==0) {
+    try {
+      const response = await $fetch('/api/user', {
+        method: 'POST',
+        body: {
+          email: email.value,
+          password: password.value,
+        },
+      })
+    } catch (e) {
+      // useNuxtApp().$toast.info('Hello World.\n I am <b>Tom</b>', {
+      //   autoClose: 5000,
+      //   dangerouslyHTMLString: true,
+      // });
+      toast.error(e.response?._data.message, {autoClose: 5000});
+    }
+  }
 }
 </script>
