8e4858ddff46e27a4ad5682bd095b07338e99dda
Gaurab19 <gaurab625@gmail.com>
2025-04-19 23:56:29 +0545

    chore:backend validation using validator.js

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -3,14 +3,34 @@
 import 'dotenv/config';
 import {drizzle} from 'drizzle-orm/mysql2';
 import {usersTable} from '../db/schema';
-import bcrypt from 'bcryptjs'
-import {e} from "prisma/build/public/assets/vendor";
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
 
 const db = drizzle(process.env.DATABASE_URL!);
 export default defineEventHandler(async (event) => {
     try {
         const body = await readBody(event)
+        if (!validator.isEmail(body.email)) {
+            throw createError({
+                statusCode: 400,
+                message: 'Please enter valid email!',
+            })
+        }
 
+        if (
+            !validator.isStrongPassword(body.password, {
+                minLength: 8,
+                minLowercase: 0,
+                minUppercase: 0,
+                minNumbers: 0,
+                minSymbols: 0,
+            })
+        ) {
+            throw createError({
+                statusCode: 400,
+                message: 'Password must be at least 8 characters long!',
+            })
+        }
         const salt = await bcrypt.genSalt(10);
         const passwordHash = await bcrypt.hash(body.password, salt);
 
@@ -27,11 +47,14 @@ export default defineEventHandler(async (event) => {
 
         return {data: users};
     }catch (e){
-        console.log('grr',e.code)
+        console.log('grr',e.code,e)
         if(e.code=='ER_DUP_ENTRY'){
             throw createError({ statusCode: 409, statusMessage: 'An email with this address already exists!' })
         }
-        return { error: 'Internal Server Error' };
+        throw createError({
+            statusCode: 400,
+            message: e?.message,
+        })
     }
 });
 
